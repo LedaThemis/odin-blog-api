7a08218f11604c37a501dff6d5b41ee0cf4b81d2
Leda <ledathemis@protonmail.com>
2022-08-10 14:18:38 +0000

    feat: add users controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+export function user_register(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function user_login(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function user_get(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
