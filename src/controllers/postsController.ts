8257ad0e0b5f593e8913a2ecd1fbd0e405f380cc
Leda <ledathemis@protonmail.com>
2022-08-10 14:35:12 +0000

    feat: add posts controller

---
+++ src/controllers/postsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+export function post_get(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function post_create(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function post_update(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function post_delete(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
