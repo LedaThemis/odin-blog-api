396c74c8f1bcc1d5aefe6bad6b51c0567db45ef5
Leda <ledathemis@protonmail.com>
2022-08-10 14:54:50 +0000

    feat: add comments controller

---
+++ src/controllers/commentsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+export function comment_get(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function comment_create(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function comment_update(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
+
+export function comment_delete(req: Request, res: Response) {
+    res.json({
+        message: 'NOT IMPLEMENTED',
+    });
+}
