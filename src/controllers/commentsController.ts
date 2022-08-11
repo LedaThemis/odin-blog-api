47ac593d4dae70d26d239ce31f2244ac7aca6d67
Leda <ledathemis@protonmail.com>
2022-08-11 12:03:14 +0000

    feat: implement comment update

--- src/controllers/commentsController.ts
+++ src/controllers/commentsController.ts
@@ -1,10 +1,70 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { Types } from 'mongoose';
+import passport from 'passport';
 
-export function comment_update(req: Request, res: Response) {
-    res.json({
-        message: 'NOT IMPLEMENTED',
-    });
-}
+import Comment from '../models/comment';
+import User from '../models/user';
+
+const validCommentId = (req: Request, res: Response, next: NextFunction) => {
+    if (!Types.ObjectId.isValid(req.params.commentId)) {
+        return res.sendStatus(400);
+    }
+
+    next();
+};
+
+const validateErrors = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array(),
+        });
+    }
+
+    next();
+};
+
+export const comment_update = [
+    validCommentId,
+    passport.authenticate('jwt', { session: false }),
+    body('content', 'Content must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    validateErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const comment = await Comment.findById(req.params.commentId);
+
+            if (!comment) {
+                return res.sendStatus(404);
+            }
+
+            const currentUser = await User.findById(req.user?.id);
+
+            if (
+                req.user?.id.toString() === comment.author.toString() ||
+                (currentUser && currentUser.isAdmin)
+            ) {
+                if (req.body.content) {
+                    comment.content = req.body.content;
+                }
+
+                const updatedComment = await comment.save();
+
+                return res.json({
+                    comment: updatedComment,
+                });
+            } else {
+                return res.sendStatus(403);
+            }
+        } catch (err) {
+            return next(err);
+        }
+    },
+];
 
 export function comment_delete(req: Request, res: Response) {
     res.json({
